e7983c47fa7d6c99e31313c578e61c3c8131311c
Arvinth07 <112890001+Arvinth07@users.noreply.github.com>
2024-03-04 16:30:56 +0530

    Update scprit.js

--- City Details/scprit.js
+++ City Details/scprit.js
@@ -1,45 +1,48 @@
 let button=document.querySelector("#button");
 let result=document.createElement("div");
-   result.id="res"
-   document.getElementById("wrapper").appendChild(result)
-button.addEventListener('click',showstats);
+result.id="res"
+document.getElementById("wrapper").appendChild(result)
+
+document.getElementById("button").addEventListener('click',showstats);
+
 function showstats(){
-   let population=0,literacyrate=0,language=0;
-   //select dropdown
-   let dropdown= document.getElementById("dropdown")
 
-   let city=dropdown.options[dropdown.selectedIndex].value
+let population=0,literacyrate=0,language=0;
+//select dropdown
+let dropdown= document.getElementById("dropdown")
 
-   switch(city)
-   {
-    case 'Bangalore' :
-        population=844367
-        literacyrate=88.71
-        language='Kannada';
-        break;
+let city=dropdown.options[dropdown.selectedIndex].value
 
-    case 'Chennai' :
-        population=4646732
-        literacyrate=90.20
-        language='Tamil';
-        break;
+switch(city)
+{
+case 'Bangalore' :
+    population=844367
+    literacyrate=88.71
+    language='Kannada';
+    break;
 
-    case 'Mumbai':
-        population=1224456
-        literacyrate=89.20
-        language='Marathi'; 
-        break;  
-        
-    case 'Delhi' :
-        population=16787942
-        literacyrate=86.20
-        language='Hindi';    
-        break;
-   }
-   let text= `The indian city of ${city} has a population of ${population}.language spoken is ${language} and  literacy rate is ${literacyrate}%`
+case 'Chennai' :
+    population=4646732
+    literacyrate=90.20
+    language='Tamil';
+    break;
 
-   
-   document.querySelector("#res").innerHTML=text;
+case 'Mumbai':
+    population=1224456
+    literacyrate=89.20
+    language='Marathi'; 
+    break;  
+    
+case 'Delhi' :
+    population=16787942
+    literacyrate=86.20
+    language='Hindi';    
+    break;
+}
+let text= `The indian city of ${city} has a population of ${population}.language spoken is ${language} and  literacy rate is ${literacyrate}%`
+
+
+document.querySelector("#res").innerHTML=text;
 
 
 
